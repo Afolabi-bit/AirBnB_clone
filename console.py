c407c659a2c9c85d53b416a8fd11776ce5863b9c
afolabi <maverickoluwatomisin@gmail.com>
2023-02-14 01:32:33 +0100

    commands

--- console.py
+++ console.py
@@ -2,21 +2,73 @@
 """ This module is for a console/ frontend of AirBnB project"""
 import cmd
 from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Class that defines the command of console.py"""
     prompt = '(hbnb) '
+    __classes = ["BaseModel"]
 
     def do_create(self, line):
         """Creates an instance
         """
+        args = line.split(" ")
         if line == '' or line is None:
             print("** class name missing **")
-        elif line not in storage.classes():
+        elif args[0] not in self.__classes:
             print("** class doesn't exist **")
         else:
-            print('exists')
+            created_class = eval(f"{args[0]}")()
+            print(created_class.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        """
+        args = line.split(" ")
+        if line == '' or line is None:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            class_id = "{}.{}".format(args[0], args[1])
+            if class_id not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[class_id])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id
+        """
+        args = line.split(" ")
+        if line == '' or line is None:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            class_id = "{}.{}".format(args[0], args[1])
+            if class_id not in storage.all():
+                print("** no instance found **")
+            else:
+                del storage.all()[class_id]
+
+    def do_all(self, line):
+        """Prints all string representation of all instances
+           based on or not on the class name
+        """
+        args = line.split(" ")
+        if line == "" or line is None:
+            print([str(values) for values in storage.all().values()])
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            one_class = [str(obj) for k, obj in storage.all().items() if k.startswith(args[0])]
+            print(one_class)
+            
 
     def do_EOF(self, line):
         """Handles End Of File character
@@ -32,7 +84,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Does nothing on Enter
         """
-        pass
+        return
 
 
 if __name__ == "__main__":
